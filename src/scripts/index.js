5d0af78322795aca9aed77a6e88f3ece448b2009
Ali Kazai <alik@digintus.com>
2020-07-16 20:21:35 +0100

    update to navbar added logo and variables file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,1 @@
 import "../styles/index.scss";
-
-// FIXME: Remove later as its not needed
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
