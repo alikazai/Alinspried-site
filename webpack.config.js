035ce5cab6cc8655a9d24dfc92dee1577b903323
Ali Kazai <alik@digintus.com>
2020-07-20 23:06:08 +0100

    setup dev server, es6 compliler with babel, eslint and copy file from src/asset to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/scripts/index.js',
-  output: { 
+  output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-   },
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
-    rules: [{
-      test: /\.scss$/i,
-      use:[
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  }
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets', // Will resolve to RepoDir/src/assets
+          to: 'assets', // Copies all files from above dest to dist/assets
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9090,
+  },
 };
